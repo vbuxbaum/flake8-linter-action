4a2ecbda34a4c8f86fa76e819cb8b384dfd3869c
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2020-11-20 11:10:50 -0300

    imp: instala dev-requirements.txt se existir

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@ set -x
 
 # Run flake8 over the student source and generate a log report
 cd /github/workspace
-python3 -m pip install -r requirements.txt
+if test -f "dev-requirements.txt" ; then
+  python3 -m pip install -r dev-requirements.txt
+else
+  python3 -m pip install -r requirements.txt
+fi
 python3 -m flake8 --append-config=setup.cfg --append-config=/home/report.cfg > /tmp/flake8.log
 
 if [ $? != 0 ]; then
