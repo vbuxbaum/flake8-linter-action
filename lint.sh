66f8c740e2c34ef38d3649f04925fd33ddba973d
Euclécio Josias Rodrigues <euclecio.rodrigues@betrybe.com>
2022-01-12 11:17:21 -0300

    imp: adc flag para não usar dir de cache

--- lint.sh
+++ lint.sh
@@ -4,9 +4,9 @@
 python3 -m venv "~/.github/venv/$INPUT_PR_NUMBER" --system-site-packages
 source "~/.github/venv/$INPUT_PR_NUMBER/bin/activate"
 if test -f "dev-requirements.txt" ; then
-  python3 -m pip install -r dev-requirements.txt
+  python3 -m pip install -r dev-requirements.txt --no-cache-dir
 else
-  python3 -m pip install -r requirements.txt
+  python3 -m pip install -r requirements.txt --no-cache-dir
 fi
 python3 -m flake8 --append-config=setup.cfg --append-config=/home/report.cfg > /tmp/flake8.log
 
